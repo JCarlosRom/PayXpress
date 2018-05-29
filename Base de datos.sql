c20d9e38f289196f9198b07e2f6c030e517eac79
JCarlosRom <jcarlosrom97@hotmail.com>
2018-05-29 14:17:03 -0500

    Basededatos

--- Base de datos.sql
+++ Base de datos.sql
@@ -1,11 +1,11 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Versión del servidor:         10.1.30-MariaDB - mariadb.org binary distribution
+-- Versión del servidor:         10.1.31-MariaDB - mariadb.org binary distribution
 -- SO del servidor:              Win32
 -- HeidiSQL Versión:             9.5.0.5196
 -- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENviernesdescuentoT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
@@ -35,13 +35,48 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   PRIMARY KEY (`IdCliente`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla pay_express.cliente: ~2 rows (aproximadamente)
+-- Volcando datos para la tabla pay_express.cliente: ~3 rows (aproximadamente)
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
 INSERT INTO `cliente` (`IdCliente`, `Nombre`, `PrimerApellido`, `SegundoApellido`, `Municipio`, `Calle`, `CodigoPostal`, `NoInterior`, `NoExterior`, `Colonia`, `RFC`, `Telefono`, `Correo`, `Empresa`) VALUES
 	(1, 'CARLOS', 'ROMERO', 'ALONSO', 'VILLA DE ALVAREZ', '16 SEPTIEMBRE', '28000', NULL, '23', 'VILLAS DE ORO', 'CARL458596', '315258', 'carlos@tec.com', NULL),
 	(2, 'ROBERTO', 'PALAZUELOS', 'LOPEZ', 'COLIMA', 'MORELOS', '28000', NULL, '565', 'CENTRO', 'ROBP398489', '45854585', 'robert@gmail.com', NULL),
 	(3, 'LAURA', 'GARCIA', 'MENDOZA', 'VILLA DE ALVAREZ', 'PRIVADA GAYTAN', '28984', NULL, '120', 'LOMAS CENTENARIO', 'LAU48U47FF', '3125458', 'lau@gmail.com', NULL);
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+
+-- Volcando estructura para procedimiento pay_express.Crear_temporal
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Crear_temporal`(
+	IN `RFC_procedure` VARCHAR(50),
+	IN `Producto_procedure` VARCHAR(50),
+	IN `Cantidad_procedure` INT,
+	IN `Tipo_pago_procedure` VARCHAR(50),
+	IN `Tipo_comprobante_procedure` VARCHAR(50)
+
+
+
+
+
+)
+BEGIN
+
+	declare precio_unitario int;
+	declare total int; 
+	
+	set precio_unitario =(select PrecioUnitario from producto where NombreProducto =  Producto_procedure);
+	
+	set total = precio_unitario*Cantidad_procedure;
+	 
+	
+	Insert into Temporal_venta (RFC, Producto, Cantidad, Tipo_pago,Tipo_comprobante,Precio_unitario, Total) values
+			(RFC_procedure,Producto_procedure, Cantidad_procedure,Tipo_pago_procedure ,Tipo_comprobante_procedure, precio_unitario, total);
+	
+	Select T.Id, T.RFC, T.Producto, T.Cantidad, T.Tipo_pago, T.Tipo_comprobante,
+			T.Cantidad*P.PrecioUnitario
+ 			from Temporal_venta T
+ 			inner join Producto P on T.Producto=P.NombreProducto;
+			
+END//
+DELIMITER ;
 
 -- Volcando estructura para tabla pay_express.empleado
 CREATE TABLE IF NOT EXISTS `empleado` (
@@ -57,7 +92,7 @@ CREATE TABLE IF NOT EXISTS `empleado` (
   PRIMARY KEY (`IdEmpleado`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla pay_express.empleado: ~2 rows (aproximadamente)
+-- Volcando datos para la tabla pay_express.empleado: ~4 rows (aproximadamente)
 /*!40000 ALTER TABLE `empleado` DISABLE KEYS */;
 INSERT INTO `empleado` (`IdEmpleado`, `Usuario`, `Nombres`, `Apellido`, `Apellido2`, `Telefono`, `Correo`, `Contraseña`, `Sucursal`) VALUES
 	(10, 'Administrador', 'Leonel', 'Ortega', 'Natera', '31254656', 'leo@gmail.com', '202cb962ac59075b964b07152d234b70', 'IS'),
@@ -73,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `estadoservicio` (
   PRIMARY KEY (`IdEstado`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla pay_express.estadoservicio: ~4 rows (aproximadamente)
+-- Volcando datos para la tabla pay_express.estadoservicio: ~5 rows (aproximadamente)
 /*!40000 ALTER TABLE `estadoservicio` DISABLE KEYS */;
 INSERT INTO `estadoservicio` (`IdEstado`, `NombreEstado`) VALUES
 	(1, 'Recibido'),
@@ -82,6 +117,17 @@ INSERT INTO `estadoservicio` (`IdEstado`, `NombreEstado`) VALUES
 	(4, 'Entregado'),
 	(5, 'Sin entregar');
 /*!40000 ALTER TABLE `estadoservicio` ENABLE KEYS */;
+
+-- Volcando estructura para procedimiento pay_express.Login
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Login`(
+	IN `Contraseña_procedure` VARCHAR(50),
+	IN `Usuario_procedure` VARCHAR(50)
+)
+BEGIN
+	Select * from empleado where Contraseña=Contraseña_procedure && Usuario=Usuario_procedure;
+END//
+DELIMITER ;
 
 -- Volcando estructura para tabla pay_express.producto
 CREATE TABLE IF NOT EXISTS `producto` (
@@ -98,37 +144,14 @@ CREATE TABLE IF NOT EXISTS `producto` (
   CONSTRAINT `FKTipoPro` FOREIGN KEY (`IdTipoProducto`) REFERENCES `producto` (`IdProducto`) ON DELETE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla pay_express.producto: ~3 rows (aproximadamente)
+-- Volcando datos para la tabla pay_express.producto: ~4 rows (aproximadamente)
 /*!40000 ALTER TABLE `producto` DISABLE KEYS */;
 INSERT INTO `producto` (`IdProducto`, `NombreProducto`, `Marca`, `Modelo`, `IdTipoProducto`, `PrecioUnitario`, `Existencia`, `Estatus`) VALUES
-	(1, '85A', 'HP', '85A', 1, 320, 9, b'1'),
+	(1, '85A', 'HP', '85A', 1, 320, 6, b'1'),
 	(2, 'TINTA EPSOPN', 'EPSON', '664', 3, 140, 5, b'1'),
 	(3, 'MOUSE', 'LOGITECH', 'M75', 2, 130, 5, b'1'),
 	(4, 'Liquido limpiador PC', 'CleanPC', 'CleanPC', 3, 50, 8, b'1');
 /*!40000 ALTER TABLE `producto` ENABLE KEYS */;
-
--- Volcando estructura para tabla pay_express.productoservicioventa
-CREATE TABLE IF NOT EXISTS `productoservicioventa` (
-  `IdProducto` int(11) DEFAULT NULL,
-  `IdServicio` int(11) DEFAULT NULL,
-  `Cantidad` int(11) NOT NULL,
-  `Descuento` int(11) DEFAULT NULL,
-  `IdVenta` int(11) NOT NULL,
-  `Importe` int(11) NOT NULL,
-  `PrecioUnitario` int(11) NOT NULL,
-  KEY `FKId_Ser` (`IdServicio`),
-  KEY `FKId_prod` (`IdProducto`),
-  KEY `FK3idventa` (`IdVenta`),
-  CONSTRAINT `FK3idventa` FOREIGN KEY (`IdVenta`) REFERENCES `venta` (`IdVenta`),
-  CONSTRAINT `FKId_Ser` FOREIGN KEY (`IdServicio`) REFERENCES `servicio` (`IdServicio`),
-  CONSTRAINT `FKId_prod` FOREIGN KEY (`IdProducto`) REFERENCES `producto` (`IdProducto`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Volcando datos para la tabla pay_express.productoservicioventa: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `productoservicioventa` DISABLE KEYS */;
-INSERT INTO `productoservicioventa` (`IdProducto`, `IdServicio`, `Cantidad`, `Descuento`, `IdVenta`, `Importe`, `PrecioUnitario`) VALUES
-	(1, NULL, 1, NULL, 1, 320, 320);
-/*!40000 ALTER TABLE `productoservicioventa` ENABLE KEYS */;
 
 -- Volcando estructura para tabla pay_express.proveedor
 CREATE TABLE IF NOT EXISTS `proveedor` (
@@ -158,17 +181,6 @@ CREATE TABLE IF NOT EXISTS `proveedorproducto` (
 /*!40000 ALTER TABLE `proveedorproducto` DISABLE KEYS */;
 /*!40000 ALTER TABLE `proveedorproducto` ENABLE KEYS */;
 
--- Volcando estructura para procedimiento pay_express.RealizarVenta
-DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `RealizarVenta`()
-BEGIN
-	declare num int;
-	
-	insert into venta values ();
-
-END//
-DELIMITER ;
-
 -- Volcando estructura para tabla pay_express.servicio
 CREATE TABLE IF NOT EXISTS `servicio` (
   `IdServicio` int(11) NOT NULL,
@@ -193,6 +205,8 @@ CREATE TABLE IF NOT EXISTS `temporal_venta` (
   `RFC` varchar(50) DEFAULT NULL,
   `Producto` varchar(50) DEFAULT NULL,
   `Cantidad` int(11) DEFAULT NULL,
+  `Precio_unitario` int(11) DEFAULT NULL,
+  `Total` int(11) DEFAULT NULL,
   `Tipo_pago` varchar(50) DEFAULT NULL,
   `Tipo_comprobante` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`Id`)
@@ -235,33 +249,88 @@ INSERT INTO `tiposervicio` (`IdTipoServicio`, `NombreTipo`) VALUES
 	(4, 'Formateo');
 /*!40000 ALTER TABLE `tiposervicio` ENABLE KEYS */;
 
+-- Volcando estructura para procedimiento pay_express.Venta
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Venta`(
+	IN `Empleado_procedure` INT,
+	IN `Cliente_procedure` INT
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
+BEGIN
+
+	DECLARE done INT DEFAULT 0;
+	Declare Producto_Procedure varchar(50);
+	Declare Total_procedure int;
+	Declare Tipo_pago_procedure varchar(50);
+	Declare Precio_unitario_procedure int;
+	Declare Cantidad_procedure int; 
+	
+	
+	Declare Temp cursor for select Producto, Total, Precio_unitario,Tipo_pago, Cantidad  from temporal_venta ;
+	
+	DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET done = 1;
+
+
+
+	Open Temp;
+	
+	REPEAT
+	
+	fetch temp into Producto_procedure, Total_procedure, Precio_unitario_procedure,Tipo_pago_procedure, Cantidad_procedure;
+	
+
+	
+	insert into venta (IdEmpleado,IdCliente,Producto,FechaVenta,SubTotal,Cantidad,IVA,Total,FormaDePago) 
+	values(Empleado_procedure,Cliente_procedure,Producto_procedure,CURDATE(),Precio_unitario_procedure,Cantidad_procedure,16,Total_procedure,Tipo_pago_procedure );
+	
+	
+	
+	UNTIL done END REPEAT;
+	
+	delete from venta where IdVenta= LAST_INSERT_ID();
+	
+	delete from temporal_venta;
+	
+	close Temp; 
+	
+
+END//
+DELIMITER ;
+
 -- Volcando estructura para tabla pay_express.venta
 CREATE TABLE IF NOT EXISTS `venta` (
   `IdVenta` int(11) NOT NULL AUTO_INCREMENT,
   `IdEmpleado` int(11) NOT NULL,
   `IdCliente` int(11) NOT NULL,
+  `Producto` varchar(50) DEFAULT NULL,
   `FechaVenta` datetime NOT NULL,
   `SubTotal` int(11) NOT NULL,
+  `Cantidad` int(11) DEFAULT NULL,
   `IVA` int(11) NOT NULL,
   `Total` int(11) NOT NULL,
   `FormaDePago` bit(1) NOT NULL,
-  PRIMARY KEY (`IdVenta`),
-  KEY `FKClient` (`IdCliente`),
-  KEY `FKEmple` (`IdEmpleado`),
-  CONSTRAINT `FKClient` FOREIGN KEY (`IdCliente`) REFERENCES `cliente` (`IdCliente`) ON DELETE NO ACTION,
-  CONSTRAINT `FKEmple` FOREIGN KEY (`IdEmpleado`) REFERENCES `empleado` (`IdEmpleado`) ON DELETE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`IdVenta`)
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
 
 -- Volcando datos para la tabla pay_express.venta: ~4 rows (aproximadamente)
 /*!40000 ALTER TABLE `venta` DISABLE KEYS */;
-INSERT INTO `venta` (`IdVenta`, `IdEmpleado`, `IdCliente`, `FechaVenta`, `SubTotal`, `IVA`, `Total`, `FormaDePago`) VALUES
-	(1, 11, 1, '2018-05-25 00:00:00', 100, 16, 116, b'1'),
-	(2, 11, 1, '2018-05-25 00:00:00', 100, 16, 116, b'1'),
-	(3, 11, 1, '2018-05-25 00:00:00', 100, 16, 116, b'1'),
-	(4, 11, 1, '2018-05-25 00:00:00', 100, 16, 116, b'1'),
-	(5, 11, 1, '2018-05-25 00:00:00', 100, 16, 116, b'1'),
-	(6, 11, 1, '2018-05-25 00:00:00', 100, 16, 116, b'1'),
-	(7, 11, 1, '2018-05-25 00:00:00', 100, 16, 116, b'1');
+INSERT INTO `venta` (`IdVenta`, `IdEmpleado`, `IdCliente`, `Producto`, `FechaVenta`, `SubTotal`, `Cantidad`, `IVA`, `Total`, `FormaDePago`) VALUES
+	(40, 0, 1, '85A', '2018-05-29 00:00:00', 320, 2, 16, 640, b'1'),
+	(42, 0, 1, '85A', '2018-05-29 00:00:00', 320, 2, 16, 640, b'1'),
+	(44, 0, 1, '85A', '2018-05-29 00:00:00', 320, 2, 16, 640, b'1'),
+	(46, 0, 1, '85A', '2018-05-29 00:00:00', 320, 2, 16, 640, b'1');
 /*!40000 ALTER TABLE `venta` ENABLE KEYS */;
 
 -- Volcando estructura para función pay_express.viernesdescuento
@@ -290,30 +359,23 @@ BEGIN
 END//
 DELIMITER ;
 
--- Volcando estructura para disparador pay_express.salida_producto
+-- Volcando estructura para disparador pay_express.venta_before_insert
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
 DELIMITER //
-CREATE TRIGGER `salida_producto` BEFORE INSERT ON `productoservicioventa` FOR EACH ROW BEGIN
-declare punit int;
-declare esta bit;
-
-select Estatus into esta from Producto where IdProducto=NEW.IdProducto;
-
-IF esta=1 THEN
-BEGIN
-Select PrecioUnitario into punit from Producto where IdProducto=NEW.IdProducto;
-
-
-set NEW.PrecioUnitario = punit;
-
-SET NEW.Importe = NEW.Cantidad * NEW.PrecioUnitario;
-
-UPDATE Producto
-set Existencia = Existencia - NEW.Cantidad
-WHERE IdProducto= NEW.IdProducto;
-END;
-END IF;
-
+CREATE TRIGGER `venta_before_insert` BEFORE INSERT ON `venta` FOR EACH ROW BEGIN
+	declare exist int;
+	declare stat int;
+	
+	set stat = (select Estatus from Producto where NombreProducto=NEW.Producto);
+	set exist= (select Existencia from producto where NombreProducto=NEW.Producto);
+	
+	IF stat!=0 AND exist>6 THEN 
+	
+		update Producto set Existencia=Exist-1 where NombreProducto= New.Producto;
+	
+	END IF;
+	
+	
 END//
 DELIMITER ;
 SET SQL_MODE=@OLDTMP_SQL_MODE;
